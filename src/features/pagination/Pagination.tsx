ded35f3046f3be9d162fa30692f86e305019c5d3
ArhDemetriy <ArhDemetriy@GMail.com>
2023-02-07 07:47:20 +0800

    init Pagination

---
+++ src/features/pagination/Pagination.tsx
@@ -0,0 +1,25 @@
+import { FC, MutableRefObject } from "react";
+import page from "./ico/page.svg";
+
+interface PaginationItemProps {
+    readonly caption: string
+}
+
+const PaginationItem: FC<PaginationItemProps> = p => {
+    return <div>
+        <img src={page} alt="" />
+        <span>{p.caption}</span>
+    </div>
+}
+
+interface PaginationProps {
+    readonly items: PaginationItemProps[]
+    readonly select: MutableRefObject<readonly number[]>
+}
+
+export const Pagination: FC<PaginationProps> = p => {
+    return <div>
+        <PaginationItem caption="По проекту"/>
+        <PaginationItem caption="Объекты"/>
+    </div>
+}

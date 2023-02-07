41b2f5d1f16f1c3672b4aee576a0dc59ade95348
ArhDemetriy <ArhDemetriy@GMail.com>
2023-02-07 13:36:19 +0800

    on Header and Pagination to Main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,15 @@
 import { FC } from "react";
+import { Header } from "../features/Header/Header";
+import { Pagination } from "../features/Pagination/Pagination";
 import { CustomTable } from "../features/CustomTable/CustomTable";
 
 interface MainProps {
 }
 
-export const Main: FC<MainProps> = p => {
+export const Main: FC<MainProps> = _ => {
     return <div>
+        <Header />
+        <Pagination />
         <CustomTable />
     </div>
 }

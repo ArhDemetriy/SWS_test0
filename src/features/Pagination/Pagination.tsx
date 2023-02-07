3025302c56f7f30a5ff6517b012643c1ac36cb88
ArhDemetriy <ArhDemetriy@GMail.com>
2023-02-07 13:25:14 +0800

    init styles in pagination

--- src/features/Pagination/Pagination.tsx
+++ src/features/Pagination/Pagination.tsx
@@ -1,25 +1,32 @@
-import { FC, MutableRefObject } from "react";
+import { FC } from "react";
+import styles from './Pagination.module.scss';
 import page from "./ico/page.svg";
+
+const SELECT = 1
+
+const ITEMS = [
+    "По проекту",
+    "Объекты",
+]
 
 interface PaginationItemProps {
     readonly caption: string
+    readonly select?: boolean
 }
 
 const PaginationItem: FC<PaginationItemProps> = p => {
-    return <div>
-        <img src={page} alt="" />
-        <span>{p.caption}</span>
+    const className = styles["pagination__item"] + (p.select ? " " + styles["pagination__item-select"] : "")
+    return <div className={className}>
+        <img src={page} className={styles["pagination__item__ico"]} alt="" />
+        <span className={styles["pagination__item__caption"]} >{p.caption}</span>
     </div>
 }
 
-interface PaginationProps {
-    readonly items: PaginationItemProps[]
-    readonly select: MutableRefObject<readonly number[]>
-}
+interface PaginationProps {}
 
 export const Pagination: FC<PaginationProps> = _ => {
-    return <div>
-        <PaginationItem caption="По проекту"/>
-        <PaginationItem caption="Объекты"/>
-    </div>
+    return <div className={styles["pagination"]} >{
+        ITEMS
+            .map((caption, i) => <PaginationItem caption={caption} select={i === SELECT} key={i} />)
+    }</div>
 }
